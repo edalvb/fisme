7cd9ccbdebe4f605409856b0af72acbdf7bc0892
Edward Alexis <edalvb@gmail.com>
2021-05-24 20:10:19 -0500

    - Update Angular - Created Strapi

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c7fb50733ab8272eacaf7418c9531c55'),
+    },
+  },
+});
